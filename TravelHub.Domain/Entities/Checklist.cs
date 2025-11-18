bba6b1c46c0381101b47a329f36bf7be1b135bb0
jerry0-3 <jerry@gregpc.pl>
2025-11-18 01:02:01 +0100

    Checklist change to fix person linking problems

--- TravelHub.Domain/Entities/Checklist.cs
+++ TravelHub.Domain/Entities/Checklist.cs
@@ -1,20 +1,54 @@
 ﻿namespace TravelHub.Domain.Entities;
 
+//public class Checklist
+//{
+//    public Dictionary<string, bool> Items { get; set; } = new();
+
+//    public void AddItem(string item, bool isCompleted = false)
+//    {
+//        Items[item] = isCompleted;
+//    }
+
+//    public void MarkComplete(string item)
+//    {
+//        if (Items.ContainsKey(item))
+//            Items[item] = true;
+//    }
+
+//    public int CompletedCount => Items.Count(x => x.Value);
+//    public int TotalCount => Items.Count;
+//}
+
 public class Checklist
 {
-    public Dictionary<string, bool> Items { get; set; } = new();
+    public List<ChecklistItem> Items { get; set; } = new();
 
-    public void AddItem(string item, bool isCompleted = false)
+    public void AddItem(string title, bool isCompleted = false)
     {
-        Items[item] = isCompleted;
+        if (Items.Any(i => i.Title == title))
+            return;
+        Items.Add(new ChecklistItem { Title = title, IsCompleted = isCompleted });
     }
 
-    public void MarkComplete(string item)
+    public void RemoveItem(string title)
     {
-        if (Items.ContainsKey(item))
-            Items[item] = true;
+        var it = Items.FirstOrDefault(i => i.Title == title);
+        if (it != null) Items.Remove(it);
     }
 
-    public int CompletedCount => Items.Count(x => x.Value);
+    public ChecklistItem? Find(string title) => Items.FirstOrDefault(i => i.Title == title);
+
+    public int CompletedCount => Items.Count(x => x.IsCompleted);
     public int TotalCount => Items.Count;
+}
+
+public class ChecklistItem
+{
+    public string Title { get; set; } = "";
+
+    public bool IsCompleted { get; set; } = false;
+
+    public string? AssignedParticipantId { get; set; }
+
+    public string? AssignedParticipantName { get; set; }
 }
